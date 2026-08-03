efbff279eea61265bffa0407da1356e027a5ff58
isabedo <isabelabedoya719@gmail.com>
2026-08-03 11:42:08 -0500

    wa

--- 8thWall/src/app.js
+++ 8thWall/src/app.js
@@ -1,7 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('..\image-targets\TargetProfe.json"'),
+      require('../image-targets/TargetProfe.json'),
     ],
   })
 }
